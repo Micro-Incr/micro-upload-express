e9a9ea160481bc57d626c6ed23a1781cac0563e9
Prashant Rayamajhi <prashantrayamajhi85@gmail.com>
2021-03-07 19:02:33 +0545

    fix port

--- server/server.ts
+++ server/server.ts
@@ -5,7 +5,7 @@ import {PORT} from './config/baseConfig';
 import connect from './utils/db';
 
 connect.then(() => {
-    app.listen(process.env.PORT || PORT, () => {
+    app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
 });
